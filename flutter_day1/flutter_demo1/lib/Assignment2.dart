751fb0f013e4175a32c6a2a7d2ff8a97b3f6d683
abhaymadanpatil31 <flutterabhaypatil@gmail.com>
2024-02-12 22:13:23 +0530

    "QuizApps"

--- flutter_day1/flutter_demo1/lib/Assignment2.dart
+++ flutter_day1/flutter_demo1/lib/Assignment2.dart
@@ -1,36 +1,82 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class Assignment2 extends StatelessWidget {
   const Assignment2({super.key});
+   int index = -1;
+  Color buttonColor() {
+    if (index == 0) {
+      return Colors.red;
+    } else {
+      return Colors.blue;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Assignment 2"),
-      ),
-      body: SizedBox(
-        height: double.infinity,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.orange,
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.amber,
-            ),
-          ],
+        title: const Text(
+          "Analysing",
+          style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.w900,
+          ),
         ),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState() {
+                    if (index == -1) {
+                      index += 1;
+                    }
+                  }
+
+                  ;
+                },
+                child: const Text("Button1"),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Button2"),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Button3"),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
